792ec1e77c281ae6ab1a8d78a9ed1c90d19364b8
ale742 <leha2017tv@gmail.com>
2025-05-22 03:17:20 +0500

    style: update styles for navigation buttons

--- src/components/home/CallToActionSection.tsx
+++ src/components/home/CallToActionSection.tsx
@@ -1,6 +1,7 @@
 import Container from '@/components/layout/Container';
 import { FaPaperPlane } from 'react-icons/fa';
 import { Button } from '../ui/button';
+import Link from 'next/link';
 
 const CallToActionSection = () => {
   return (
@@ -14,17 +15,19 @@ const CallToActionSection = () => {
           Присоединяйтесь к тысячам авиационных энтузиастов и профессионалов.
           Найдите интересующую вас информацию прямо сейчас!
         </p>
-        <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4">
-          <Button
-            className="bg-white text-blue-700 hover:bg-gray-100 shadow-lg"
+         <div className="flex flex-col sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-3">
+          <Link
+            href="/aircraft"
+            className="inline-flex items-center justify-center px-4 py-2rounded-md text-sm font-medium bg-white text-blue-600 hover:bg-gray-200  dark:bg-gray-200 dark:text-blue-700 dark:hover:bg-gray-300 transition-colors duration-150"
           >
             Искать самолеты
-          </Button>
-          <Button
-            className="bg-transparent border border-white text-white hover:bg-white hover:text-blue-700 shadow-lg"
+          </Link>
+          <Link
+            href="/flight-routes"
+            className="inline-flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium border border-blue-300 text-white hover:bg-blue-300 hover:text-blue-700 dark:border-blue-400 dark:text-gray-100 dark:hover:bg-blue-400 dark:hover:text-blue-800 transition-colors duration-150"
           >
             Маршруты полетов
-          </Button>
+          </Link>
         </div>
       </Container>
     </section>
